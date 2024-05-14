8ed9d7fd9af398e34fdfee3f33d96693f7224ce6
Dani <Dani@DESKTOP-GNLV37F>
2024-05-14 17:15:03 +0200

    commit de repaso

---
+++ src/main/java/com/mycompany/del1/_6/Del1100_6.java
@@ -0,0 +1,10 @@
+package com.mycompany.del1._6;
+public class Del1100_6 {
+
+    public static void main(String[] args) {
+        for (int i=2;i<100;i+=2){
+             System.out.println(i);
+        }
+
+    }
+}
